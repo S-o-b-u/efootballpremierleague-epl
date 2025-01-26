ecef7907fd62eebc70cf8df5b9b6e45cfc621eae
Souvik Rahut <souvikrahutofficial@gmail.com>
2025-01-26 22:14:57 +0530

    padding

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
   return (
     <>
     <div className="flex justify-center items-center m-auto w-full min-h-screen  overflow-hidden scrollbar-hide">
-      <div className="flex pt-20 mt-5 mb-6 justify-center flex-col gap-10 md:mt-10">
+      <div className="flex pt-20 px-3 mt-5 mb-6 justify-center flex-col gap-10 md:mt-10">
         <Testimonials />
         <EmojiFeedback />
       </div>
