750f848dee1c2ea903cc961a92800e1c2f884426
Souvik Rahut <souvikrahutofficial@gmail.com>
2025-06-12 21:33:41 +0530

    404

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,14 +1,20 @@
-import Footer from '@/components/Footer'
-import RegisterForm from '@/components/RegisterForm'
-import React from 'react'
+import Footer from "@/components/Footer";
+import FuzzyText from "@/components/FuzzyText";
+import RegisterForm from "@/components/RegisterForm";
+import React from "react";
 
 const page = () => {
   return (
-    <div className="min-h-screen">
-    <RegisterForm />
-    <Footer/>
-  </div>
-  )
-}
+    <>
+      <div className="h-screen w-full flex flex-col gap-8 items-center justify-center">
+        {/* <RegisterForm /> */}
+        <FuzzyText baseIntensity={0.2} hoverIntensity={0.2} enableHover={true}>
+          404
+        </FuzzyText>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default page
+export default page;
